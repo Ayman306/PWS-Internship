2ad4312e2a7db99d5b81561265b6c340d74a0efb
Ayman <57653097+Ayman306@users.noreply.github.com>
2022-08-24 18:09:37 +0530

    Add files via upload

---
+++ Angular/Ex3 Task Tracker/src/app/mock-tasks.ts
@@ -0,0 +1,88 @@
+import { Task } from "./Task"
+
+export const TASKS:Task[]=[
+    {
+        id:1,
+        text:'Doctor',
+        day:'May 5th at 2:30pm',
+        reminder:true
+    },
+    {
+        id:2,
+        text:'Doctor2',
+        day:'May 5th at 2:30pm',
+        reminder:false
+    },
+    {
+        id:3,
+        text:'Doctor3',
+        day:'May 5th at 2:30pm',
+        reminder:true
+    },
+    {
+        id:4,
+        text:'Doctor4',
+        day:'May 5th at 2:30pm',
+        reminder:true
+    },
+    {
+        id:5,
+        text:'Doctor5',
+        day:'May 5th at 2:30pm',
+        reminder:true
+    },
+    {
+        id:6,
+        text:'Doctor6',
+        day:'May 5th at 2:30pm',
+        reminder:false
+    },
+    {
+        id:7,
+        text:'Doctor7',
+        day:'May 5th at 2:30pm',
+        reminder:true
+    },
+    {
+        id:8,
+        text:'Doctor8',
+        day:'May 5th at 2:30pm',
+        reminder:false
+    },
+    {
+        id:9,
+        text:'Doctor9',
+        day:'May 5th at 2:30pm',
+        reminder:true
+    },
+    {
+        id:10,
+        text:'Doctor10',
+        day:'May 5th at 2:30pm',
+        reminder:false
+    },
+    {
+        id:11,
+        text:'Doctor11',
+        day:'May 5th at 2:30pm',
+        reminder:true
+    },
+    {
+        id:12,
+        text:'Doctor12',
+        day:'May 5th at 2:30pm',
+        reminder:true
+    },
+    {
+        id:13,
+        text:'Doctor13',
+        day:'May 5th at 2:30pm',
+        reminder:true
+    },
+    {
+        id:14,
+        text:'Doctor14',
+        day:'May 5th at 2:30pm',
+        reminder:true
+    },
+]
